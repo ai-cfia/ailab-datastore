86c93a96b5db33fd081218e434fe1fdd979005e2
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-04-09 19:16:44 +0000

    Issue #13: fix bytebase errors

--- datastore/db/bytebase/nachet-schema-005##fsdh##20240402195530##ddl.sql
+++ datastore/db/bytebase/nachet-schema-005##fsdh##20240402195530##ddl.sql
@@ -4,34 +4,34 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "nachet_0.0.5";
 
 SET search_path TO "nachet_0.0.5";
 
-CREATE TABLE users (
+CREATE TABLE "nachet_0.0.5".users (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE picture_set (
+CREATE TABLE "nachet_0.0.5".picture_set (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     picture_set JSON,
     owner_id uuid REFERENCES users(id),
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE pictures (
+CREATE TABLE "nachet_0.0.5".pictures (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     picture_set_id uuid REFERENCES picture_set(id),
     picture JSON,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE seeds (
+CREATE TABLE "nachet_0.0.5".seeds (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     metadata JSON,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE picture_seed (
+CREATE TABLE "nachet_0.0.5".picture_seed (
     id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
     picture_id uuid REFERENCES picture(id),
     seed_id uuid REFERENCES seeds(id),
