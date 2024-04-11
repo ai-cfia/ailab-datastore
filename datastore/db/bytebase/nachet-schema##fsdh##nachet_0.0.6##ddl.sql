2667280ddbf851cf6812bd9860d918362cfdabed
Francois-Werbrouck <francois.werbrouck.1@ens.etsmtl.ca>
2024-04-11 11:22:28 -0400

    Issue #13: bytebase 006

--- datastore/db/bytebase/nachet-schema##fsdh##nachet_0.0.6##ddl.sql
+++ datastore/db/bytebase/nachet-schema##fsdh##nachet_0.0.6##ddl.sql
@@ -3,8 +3,8 @@ DO
 $do$
 BEGIN
   IF (NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nachet_0.0.5')) THEN
-    CREATE SCHEMA "nachet_0.0.5";
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "nachet_0.0.5";
+    CREATE SCHEMA "nachet_0.0.6";
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "nachet_0.0.6";
     --Table creation file 
   END IF;
 END
--- datastore/db/bytebase/nachet-tables##fsdh##0.0.6##ddl.sql
+++ datastore/db/bytebase/nachet-tables##fsdh##0.0.6##ddl.sql
@@ -2,37 +2,37 @@
 DO
 $do$
 BEGIN
-  IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nachet_0.0.5')) THEN
+  IF (EXISTS(SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nachet_0.0.5')) THEN
     --Table creation file 
-    CREATE TABLE "nachet_0.0.5".users (
+    CREATE TABLE "nachet_0.0.6".users (
         id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
         email VARCHAR(255) NOT NULL,
         registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
-    CREATE TABLE "nachet_0.0.5".picture_set (
+    CREATE TABLE "nachet_0.0.6".picture_set (
         id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
         picture_set JSON,
         owner_id uuid REFERENCES users(id),
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
-    CREATE TABLE "nachet_0.0.5".pictures (
+    CREATE TABLE "nachet_0.0.6".pictures (
         id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
         picture_set_id uuid REFERENCES picture_set(id),
         picture JSON,
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
-    CREATE TABLE "nachet_0.0.5".seeds (
+    CREATE TABLE "nachet_0.0.6".seeds (
         id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
         metadata JSON,
         name VARCHAR(255) NOT NULL,
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
-    CREATE TABLE "nachet_0.0.5".picture_seed (
+    CREATE TABLE "nachet_0.0.6".picture_seed (
         id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
         picture_id uuid REFERENCES picture(id),
         seed_id uuid REFERENCES seeds(id),
