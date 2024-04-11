764585302490325e54225449cb11d0b7f1019924
Francois-Werbrouck <francois.werbrouck.1@ens.etsmtl.ca>
2024-04-11 11:36:31 -0400

    Issue #13: schema 0.0.7 bytebase

--- datastore/db/bytebase/nachet-schema##fsdh##0.0.7##ddl.sql
+++ datastore/db/bytebase/nachet-schema##fsdh##0.0.7##ddl.sql
@@ -4,38 +4,37 @@ $do$
 BEGIN
   IF (NOT EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nachet_0.0.7')) THEN
     CREATE SCHEMA "nachet_0.0.7";
-    CREATE EXTENSION "uuid-ossp" WITH SCHEMA "nachet_0.0.7";
     --Table creation file
     CREATE TABLE "nachet_0.0.7".users (
-        id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+        id uuid DEFAULT "uuid_".uuid_generate_v4() PRIMARY KEY,
         email VARCHAR(255) NOT NULL,
         registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
     );
 
     CREATE TABLE "nachet_0.0.7".picture_set (
-        id uuid DEFAULT "nachet_0.0.7".uuid_generate_v4() PRIMARY KEY,
+        id uuid DEFAULT "uuid_".uuid_generate_v4() PRIMARY KEY,
         picture_set JSON,
         owner_id uuid REFERENCES users(id),
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     CREATE TABLE "nachet_0.0.7".pictures (
-        id uuid DEFAULT "nachet_0.0.7".uuid_generate_v4() PRIMARY KEY,
+        id uuid DEFAULT "uuid_".uuid_generate_v4() PRIMARY KEY,
         picture_set_id uuid REFERENCES picture_set(id),
         picture JSON,
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     CREATE TABLE "nachet_0.0.7".seeds (
-        id uuid DEFAULT "nachet_0.0.7".uuid_generate_v4() PRIMARY KEY,
+        id uuid DEFAULT "uuid_".uuid_generate_v4() PRIMARY KEY,
         metadata JSON,
         name VARCHAR(255) NOT NULL,
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
     CREATE TABLE "nachet_0.0.7".picture_seed (
-        id uuid DEFAULT "nachet_0.0.7".uuid_generate_v4() PRIMARY KEY,
+        id uuid DEFAULT "uuid_".uuid_generate_v4() PRIMARY KEY,
         picture_id uuid REFERENCES picture(id),
         seed_id uuid REFERENCES seeds(id),
         upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
