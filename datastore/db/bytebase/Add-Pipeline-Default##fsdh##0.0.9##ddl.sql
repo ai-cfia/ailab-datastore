0fb29d9587fe1cea5292bdedf7e4714b02a8884d
Francois-Werbrouck <francois.werbrouck.1@ens.etsmtl.ca>
2024-04-23 16:57:56 -0400

    Issue #23: Nachet_0.0.10

--- datastore/db/bytebase/Add-Pipeline-Default##fsdh##0.0.9##ddl.sql
+++ datastore/db/bytebase/Add-Pipeline-Default##fsdh##0.0.9##ddl.sql
@@ -5,7 +5,7 @@ BEGIN
 IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nachet_0.0.9')) THEN
 
     CREATE TABLE "nachet_0.0.9"."user" (
-        "id" uuid DEFAULT "uuid_".uuid_generate_v4() PRIMARY KEY,
+        "id" uuid DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
         "email" VARCHAR(255) NOT NULL,
         "registration_date" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -31,9 +31,8 @@ IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nache
     );
 
     INSERT INTO "nachet_0.0.9"."object_type" ("name") VALUES ('seed');
-    
-    CREATE TABLE "nachet_0.0.9"."seed" (
-        "id" uuid NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
+      CREATE TABLE "nachet_0.0.9"."seed" (
+        "id" uuid NOT NULL DEFAULT uuid_.uuid_.generate_v4() PRIMARY KEY,
         "name" text NOT NULL,
         "object_type_id" integer GENERATED ALWAYS AS (1) STORED
     );
@@ -46,22 +45,23 @@ IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nache
  
     CREATE TABLE "nachet_0.0.9"."model" (
         "id" uuid NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
-        "name" text NOT NULL,
-        "active_version" uuid NOT NULL REFERENCES "nachet_0.0.9".model_version(id)
+        "name" text NOT NULL
     );
     
     CREATE TABLE "nachet_0.0.9"."model_version" (
-        "id" integer NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
+        "id" uuid NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
         "model_id" uuid NOT NULL REFERENCES "nachet_0.0.9".model(id),
         "data" json NOT NULL,
-        "version" text NOT NULL DEFAULT '0.0.0'
+        "version" text NOT NULL
     );
     
+    Alter table "nachet_0.0.9".model ADD "active_version" uuid NOT NULL REFERENCES "nachet_0.0.9".model_version(id);
+
     CREATE TABLE "nachet_0.0.9"."object" (
         "id" uuid NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
         "box_metadata" json NOT NULL,
         "inference_id" uuid NOT NULL REFERENCES "nachet_0.0.9".inference(id),
-        "type_id" uuid NOT NULL REFERENCES "nachet_0.0.9".obj_type(id),
+        "type_id" integer NOT NULL REFERENCES "nachet_0.0.9".object_type(id),
         "verified_id" uuid NOT NULL,
         "valid" boolean NOT NULL DEFAULT true,
         "top_inference_id" uuid NOT NULL
@@ -75,7 +75,7 @@ IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nache
         "is_default" boolean NOT NULL DEFAULT false
     );
 
-    CREATE UNIQUE INDEX "nachet_0.0.9"."pipeline_default" ON "nachet_0.0.9".pipeline("is_default") where is_default=true;
+    CREATE UNIQUE INDEX "nachet_0.0.9_pipeline_default" ON "nachet_0.0.9".pipeline ("is_default") where is_default=true;
     
     CREATE TABLE "nachet_0.0.9"."pipeline_model" (
         "id" uuid NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
@@ -87,7 +87,8 @@ IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nache
         "id" uuid NOT NULL DEFAULT uuid_.uuid_generate_v4() PRIMARY KEY,
         "seed_id" uuid NOT NULL REFERENCES "nachet_0.0.9".seed(id),
         "object_id" uuid NOT NULL REFERENCES "nachet_0.0.9".object(id)
-    );
+    );  
+
 END IF;
 END
 $do$
