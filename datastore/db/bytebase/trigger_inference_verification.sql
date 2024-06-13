60df62db10189fef3f37fe287c589ab896cd73b7
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-06-13 19:06:03 +0000

    Issue #41: Fix return error

--- datastore/db/bytebase/trigger_inference_verification.sql
+++ datastore/db/bytebase/trigger_inference_verification.sql
@@ -10,6 +10,7 @@ CREATE OR REPLACE FUNCTION verified_inference() RETURNS TRIGGER LANGUAGE plpgsql
               ON so.object_id = obj.verified_id  
           WHERE obj.inference_id = NEW.id and obj.verified_id is not null;
     END IF;
+    RETURN NEW;
   END;
 $$;
 
