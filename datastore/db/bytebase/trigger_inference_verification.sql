95cfbb6a37a9e04d02ba94cb64bc6b8e4c04f566
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-06-14 14:34:07 +0000

    Issue #41: Fix join and insert

--- datastore/db/bytebase/trigger_inference_verification.sql
+++ datastore/db/bytebase/trigger_inference_verification.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE FUNCTION verified_inference() RETURNS TRIGGER LANGUAGE plpgsql AS $$
   BEGIN
     IF NEW.verified = true THEN
-        INSERT INTO picture_seed (seed_id, picture_id)
+        INSERT INTO picture_seed (picture_id, seed_id)
           SELECT 
             New.picture_id, 
             so.seed_id  
           FROM object obj 
             LEFT JOIN seed_obj so 
-              ON so.object_id = obj.verified_id  
+              ON so.id = obj.verified_id  
           WHERE obj.inference_id = NEW.id and obj.verified_id is not null;
     END IF;
     RETURN NEW;
