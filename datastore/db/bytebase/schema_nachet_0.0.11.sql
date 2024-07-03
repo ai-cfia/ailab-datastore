c70a6b7c6081f946b6a7180cfdb53aae7e6343e4
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-07-03 21:36:59 +0000

    Issue #41 : update trigger in schema

--- datastore/db/bytebase/schema_nachet_0.0.11.sql
+++ datastore/db/bytebase/schema_nachet_0.0.11.sql
@@ -139,25 +139,26 @@ IF (EXISTS (SELECT 1 FROM information_schema.schemata WHERE schema_name = 'nache
     );  
 
     CREATE OR REPLACE FUNCTION verified_inference() RETURNS TRIGGER LANGUAGE plpgsql AS $$
-        BEGIN
-            IF NEW.verified = true THEN
-                INSERT INTO picture_seed (seed_id, picture_id)
-                  SELECT 
-                    New.picture_id, 
-                    so.seed_id  
-                  FROM object obj 
-                    LEFT JOIN seed_obj so 
-                      ON so.object_id = obj.verified_id  
-                  WHERE obj.inference_id = NEW.id and obj.verified_id is not null;
-            END IF;
-        END;
-    $$;
+  BEGIN
+    IF NEW.verified = true THEN
+        INSERT INTO picture_seed (picture_id, seed_id)
+          SELECT 
+            New.picture_id, 
+            so.seed_id  
+          FROM object obj 
+            LEFT JOIN seed_obj so 
+              ON so.id = obj.verified_id  
+          WHERE obj.inference_id = NEW.id and obj.verified_id is not null;
+    END IF;
+    RETURN NEW;
+  END;
+$$;
 
-    CREATE TRIGGER verified_inference_trigger 
-    AFTER UPDATE ON inference
-    FOR EACH ROW 
-        WHEN (NEW.verified = true)
-    EXECUTE FUNCTION verified_inference();
+CREATE TRIGGER verified_inference_trigger 
+AFTER UPDATE ON inference
+FOR EACH ROW 
+WHEN (NEW.verified = true)
+EXECUTE FUNCTION verified_inference();
 
     INSERT INTO "nachet_0.0.11".seed(name) VALUES
     ('Brassica napus'),
