ce0e6ded2589a52b20effa1519c742130ee0d974
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-07-19 18:13:20 +0000

    issue #77: fix tests

--- tests/FertiScan/test_datastore.py
+++ tests/FertiScan/test_datastore.py
@@ -2,6 +2,9 @@
 This is a test script for the highest level of the datastore packages. 
 It tests the functions in the __init__.py files of the datastore packages.
 """
+import asyncio
+import io
+from PIL import Image
 import unittest
 import json
 import datastore.db.__init__ as db
@@ -9,6 +12,11 @@ import datastore.__init__ as datastore
 import datastore.FertiScan as FertiScan
 import datastore.db.metadata.validator as validator
 from copy import deepcopy
+import os
+
+BLOB_CONNECTION_STRING = os.environ["NACHET_STORAGE_URL_TESTING"]
+if BLOB_CONNECTION_STRING is None or BLOB_CONNECTION_STRING == "":
+    raise ValueError("NACHET_STORAGE_URL_TESTING is not set")
 
 DB_CONNECTION_STRING = os.environ.get("FERTISCAN_DB_URL")
 if DB_CONNECTION_STRING is None or DB_CONNECTION_STRING == "":
@@ -18,19 +26,40 @@ DB_SCHEMA = os.environ.get("FERTISCAN_SCHEMA_TESTING")
 if DB_SCHEMA is None or DB_SCHEMA == "":
     raise ValueError("FERTISCAN_SCHEMA_TESTING is not set")
 
+BLOB_ACCOUNT = os.environ["NACHET_BLOB_ACCOUNT_TESTING"]
+if BLOB_ACCOUNT is None or BLOB_ACCOUNT == "":
+    raise ValueError("NACHET_BLOB_ACCOUNT is not set")
+
+BLOB_KEY = os.environ["NACHET_BLOB_KEY_TESTING"]
+if BLOB_KEY is None or BLOB_KEY == "":
+    raise ValueError("NACHET_BLOB_KEY is not set")
+
 class TestDatastore(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
-        self.analysis_json = json.loads('tests/analyse.json')
+        with open('tests/FertiScan/analyse.json', 'r') as file:
+            self.analysis_json = json.load(file)
 
-        con = db.connect_db(DB_CONNECTION_STRING)
-        self.cursor = con.cursor()
-        db.create_search_path(con, self.cursor, DB_SCHEMA)
+        self.con = db.connect_db(DB_CONNECTION_STRING, DB_SCHEMA)
+        self.cursor = self.con.cursor()
+        db.create_search_path(self.con, self.cursor, DB_SCHEMA)
 
         self.user_email = 'test@email'
-        datastore.new_user(self.cursor, self.user_email)
+        self.user_obj= asyncio.run(datastore.new_user(self.cursor,self.user_email,BLOB_CONNECTION_STRING,'test-user'))
+        
 
-        self.user_id = datastore.get_user_id(self.cursor, self.user_email)
-        self.container_client = datastore.get_user_container_client(self.user_id, 'test-user')
+        self.user_id=datastore.User.get_id(self.user_obj) 
+        self.container_client = asyncio.run(datastore.get_user_container_client(
+            user_id=self.user_id,
+            storage_url=BLOB_CONNECTION_STRING,
+            account=BLOB_ACCOUNT,
+            key=BLOB_KEY,
+            tier='test-user'))
+        
+        self.image = Image.new("RGB", (1980, 1080), "blue")
+        self.image_byte_array = io.BytesIO()
+        self.image.save(self.image_byte_array, format="TIFF")
+        self.pic_encoded = self.image.tobytes()
+
 
         
     def tearDown(self):
@@ -39,6 +68,7 @@ class TestDatastore(unittest.IsolatedAsyncioTestCase):
         db.end_query(self.con, self.cursor)
 
     def test_register_analysis(self):
-        analysis = FertiScan.register_analysis(self.cursor, self.container_client, self.user_id, self.analysis_json)
+        analysis = asyncio.run(FertiScan.register_analysis(self.cursor, self.container_client, self.user_id,[self.pic_encoded,self.pic_encoded] ,self.analysis_json))
         self.assertIsNotNone(analysis)
-        self.assertTrue(validator.is_valid_uuid(analysis['inspection_id']))
+        print(analysis)
+        self.assertTrue(validator.is_valid_uuid(analysis.inspection_id))
