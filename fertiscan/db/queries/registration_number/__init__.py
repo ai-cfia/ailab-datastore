6a7af1dc559d8b1e44efdfbeb7dfc6805d543ce7
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-11-06 22:01:52 +0000

    Issue #154: add querie module for registration_number

---
+++ fertiscan/db/queries/registration_number/__init__.py
@@ -0,0 +1,74 @@
+"""
+This module represent the function for the registration_number_information table
+
+"""
+
+from psycopg import Cursor, sql
+
+from uuid import UUID
+
+from fertiscan.db.queries.errors import (
+    RegistrationNumberCreationError,
+    RegistrationNumberNotFoundError,
+    RegistrationNumberQueryError,
+    RegistrationNumberRetrievalError,
+    handle_query_errors,
+)
+
+
+@handle_query_errors(RegistrationNumberCreationError)
+def new_registration_number(
+    cursor: Cursor,
+    registration_number,
+    label_id: UUID,
+    is_an_ingredient: bool,
+    read_name: str = None,
+    edited=False,
+):
+    """
+    This function creates a new registration_number in the database.
+    Parameters:
+    - cursor (cursor): The cursor of the database.
+    - registration_number (str): The registration number of the product.
+    - label_id (uuid): The UUID of the label_information.
+    - is_an_ingredient (bool): The status of the registration number.
+    - edited (bool): The edited status of the registration number.
+    Returns:
+    - The UUID of the new registration number.
+    """
+    query = sql.SQL(
+        """
+        SELECT new_registration_number(%s, %s, %s, %s, %s);
+    """
+    )
+    cursor.execute(
+        query, (registration_number, label_id, is_an_ingredient, read_name, edited)
+    )
+    if result := cursor.fetchone():
+        return result[0]
+    raise RegistrationNumberCreationError(
+        "Failed to create Registration Number. No data returned."
+    )
+
+
+@handle_query_errors(RegistrationNumberRetrievalError)
+def get_registration_numbers_json(cursor: Cursor, label_id: UUID):
+    """
+    This function gets the registration numbers from the database.
+    Parameters:
+    - cursor (cursor): The cursor of the database.
+    - label_id (uuid): The UUID of the label_information.
+    Returns:
+    - The registration numbers of the label.
+    """
+    query = sql.SQL(
+        """
+        SELECT get_registration_numbers_json(%s);
+    """
+    )
+    cursor.execute(query, (label_id,))
+    if result := cursor.fetchone():
+        return result[0]
+    raise RegistrationNumberRetrievalError(
+        "Failed to get Registration Numbers with the given label_id. No data returned."
+    )
