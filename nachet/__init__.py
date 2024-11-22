d4926de364eea20c60146d9a4bab6870cd8b5a04
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2024-11-22 22:19:14 +0000

    .

--- nachet/__init__.py
+++ nachet/__init__.py
@@ -72,7 +72,6 @@ async def upload_picture_unknown(
     - container_client: The container client of the user.
     """
     try:
-        raise BlobUploadError("entering upload_picture_unknown")
         print(f"User ID: {user_id}")
         print(f"Picture Set ID: {picture_set_id}")
         print(f"Folder Name: {folder_name}")
