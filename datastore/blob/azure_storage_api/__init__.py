5ed34ef93df5813ad658f26eb7f0c30d67edcbac
Joseffus Santos <joseffus.santos@inspection.gc.ca>
2025-03-12 02:32:15 +0000

    Re-add debug print statement to mount_container function

--- datastore/blob/azure_storage_api/__init__.py
+++ datastore/blob/azure_storage_api/__init__.py
@@ -123,6 +123,7 @@ async def mount_container(
     - container_client: the container client object
     """
     try:
+        print(f"initializing blob_service_client")
         blob_service_client = BlobServiceClient.from_connection_string(
             conn_str=connection_string, credential=credentials
         )
